efe9f21e19418898ce9b12b4fb58808e54616b04
Justin <jtat5912@gmail.com>
2022-08-07 02:23:40 -0700

    Trying to fix buggy header icon links on deployed ghpages

--- portfolio-angular/src/data/icon-links.ts
+++ portfolio-angular/src/data/icon-links.ts
@@ -1,14 +1,14 @@
 export const ICON_LINKS = [
   {
-    'iconUrl' : '/assets/icons/github-icon.png',
+    'iconUrl' : 'assets/icons/github-icon.png',
     'link' : 'https://github.com/justin-tat'
   },
   {
-    'iconUrl': '/assets/icons/gmail-icon.png',
-    'link' : '/assets/icons/gmail-icon.png'
+    'iconUrl': 'assets/icons/gmail-icon.png',
+    'link' : 'assets/icons/gmail-icon.png'
   },
   {
-    'iconUrl' : '/assets/icons/linked-in-icon.png',
+    'iconUrl' : 'assets/icons/linked-in-icon.png',
     'link' : 'https://www.linkedin.com/in/justin-sh-tat/'
   }
 ];
